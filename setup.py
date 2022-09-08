58543d13bb3d4a57dba7ebc365b543f79c617f1f
BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>
2022-09-08 11:50:33 +0200

    Fix version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="licensevalidator",
-    version="0.1.0",
+    version="1.2.0",
     description="A validator for dependency licenses",
     long_description=long_description,
     long_description_content_type="text/markdown",
