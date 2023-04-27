a08de39897907642b60d56ffdb06deadb87806e2
kse3hi <kse3hi@bosch.com>
2023-04-27 11:19:44 +0000

    Release version v1.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="licensevalidator",
-    version="1.2.2",
+    version="1.2.3",
     description="A validator for dependency licenses",
     long_description=long_description,
     long_description_content_type="text/markdown",
